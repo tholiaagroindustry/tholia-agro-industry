fb9afe4c68a003b48d5056ad931d72bdfeaf97c6
tholiaagroindustry <tholiasahil083@gmail.com>
2026-08-01 18:09:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,16 @@ behavior:"smooth"
 });
 
 }
+const slides=document.querySelectorAll(".slide");
+
+let currentSlide=0;
+
+setInterval(()=>{
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide=(currentSlide+1)%slides.length;
+
+slides[currentSlide].classList.add("active");
+
+},5000);
