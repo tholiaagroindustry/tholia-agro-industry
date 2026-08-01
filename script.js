938a4eac99b6526e89611a1f055897acc7048606
tholiaagroindustry <tholiasahil083@gmail.com>
2026-08-01 16:43:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function toggleMenu() {
 document.getElementById("navMenu").classList.toggle("active");
 }
+function toggleMenu() {
+document.getElementById("navMenu").classList.toggle("active");
+}
+
+document.getElementById("dealerForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let name=document.getElementById("name").value;
+let mobile=document.getElementById("mobile").value;
+let city=document.getElementById("city").value;
+let cattle=document.getElementById("cattle").value;
+let message=document.getElementById("message").value;
+
+let text=`New Dealer Inquiry
+
+Name: ${name}
+Mobile: ${mobile}
+Village/City: ${city}
+Cattle: ${cattle}
+
+Message:
+${message}`;
+
+window.open("https://wa.me/919813246435?text="+encodeURIComponent(text),"_blank");
+
+});
